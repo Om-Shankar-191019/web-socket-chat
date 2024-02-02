dd898e617a2f3cb9c633483e85fe095a86174536
om shankar <om8003golu@gmail.com>
2024-02-02 17:35:57 +0530

    login/signup starter pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 const App = () => {
   return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className="h-screen">
+      {/* <Login /> */}
+      <SignUp />
     </div>
   );
 };
