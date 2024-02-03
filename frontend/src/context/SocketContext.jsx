533e0c1a1ae1c61f0967073d149aca63feb4707b
om shankar <om8003golu@gmail.com>
2024-02-03 22:27:29 +0530

    socket io set up at frontend via context api

---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+
+const SocketContext = createContext();
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000");
+      setSocket(socket);
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
