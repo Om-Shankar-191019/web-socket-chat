b4938e8f7f5f923ac1c75d83223054205a0144c4
om shankar <om8003golu@gmail.com>
2024-02-03 17:08:46 +0530

    all user fetched at sidebar

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 const useConversation = create((set) => ({
   selectedConversation: null,
-  setSelecetedConversation: (selectedConversation) =>
+  setSelectedConversation: (selectedConversation) =>
     set({ selectedConversation }),
   messages: [],
   setMessages: (messages) => set({ messages }),
